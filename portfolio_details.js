0c97ba1a247284f0d951329661545d0084ef4e14
Karol Dwojak <karool.dwojak@gmail.com>
2024-08-22 13:59:19 +0200

    improve readability - partial

--- portfolio_details.js
+++ portfolio_details.js
@@ -5,10 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const details = icon.nextElementSibling;
     if (details.style.display === "none" || details.style.display === "") {
       details.style.display = "block";
-      icon.style.color = "#ff0000"; // Change icon color when active
     } else {
       details.style.display = "none";
-      icon.style.color = "#4CAF50"; // Revert icon color
     }
   };
 });
