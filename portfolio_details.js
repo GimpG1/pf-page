77b9db70abb12058219f3a2cabfca6dc6172afe1
Karol Dwojak <karool.dwojak@gmail.com>
2024-09-11 17:13:59 +0200

    ux changes

--- portfolio_details.js
+++ portfolio_details.js
@@ -2,11 +2,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Function to show or hide details
   window.showDetails = function (icon) {
-    const details = icon.nextElementSibling;
+    let rootElement = icon.parentElement;
+    let imgElement = icon.firstElementChild;
+    let details=  rootElement.querySelector('.hidden-details');
     if (details.style.display === "none" || details.style.display === "") {
       details.style.display = "block";
+      imgElement.src = "./images/up-arrow.png"
     } else {
       details.style.display = "none";
+      imgElement.src = "./images/down-arrow.png"
     }
   };
 });
